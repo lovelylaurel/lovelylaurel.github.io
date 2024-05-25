a418945890559effd084465947cdaa8e5dbf0e4a
lovelylaurel <lovelylaurel07@gmail.com>
2024-05-25 12:30:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const btnLike1 = document.getElementById("btnLike1")
 const btnLike2 = document.getElementById("btnLike2") 
-const countLikes1 = document.getElementById("countLike1")
-const countLikes2 = document.getElementById("countLike2")
+const countLikes1 = document.getElementById("countLikes1")
+const countLikes2 = document.getElementById("countLikes2")
 
 function hitLikes1() {
     let totalLikes1:number = parseInt(countLikes1.value) + 1
