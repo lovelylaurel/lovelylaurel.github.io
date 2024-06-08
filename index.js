4a2a0ff4bb04283b1d15d6cf6d5e5b063c6d2d47
lovelylaurel <lovelylaurel07@gmail.com>
2024-06-08 11:25:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -36,6 +36,6 @@ const submit = document.getElementById("submit")
 const comment = document.getElementById("comment")
 const commentbox = document.getElementById("commentbox")
 function submitComment(){
-  commentbox.textContent +=comment.value.toString() + "\n"
+  commentbox.textContent +=comment1.value.toString() + "\n"
 }
 submit.addEventListener("click",submitComment)
